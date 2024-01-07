5e0bf731bff00a1245ea2688d9ba9eb6968d0b53
kendaru10 <jorge.huamanimontes@gmail.com>
2024-01-07 15:22:37 -0500

    Agregar archivos de proyecto.

---
+++ CapaNegocio/CN_Producto.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Producto
+    {
+        private CD_Producto objCapaDato = new CD_Producto();
+        public List<Producto> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombre) || string.IsNullOrWhiteSpace(obj.Nombre))
+            {
+                Mensaje = "El nombre del producto no puede ser vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripción del producto no puede ser vacío";
+            }
+            else if (obj.oMarca.IdMarca == 0)
+            {
+                Mensaje = "Debe seleccionar una Marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Debe seleccionar una Categoria";
+            }
+            else if (obj.Precio == 0)
+            {
+                Mensaje = "Debe ingresar el precio del producto";
+            }
+            else if (obj.Stock == 0)
+            {
+                Mensaje = "Debe ingresar el Stock del producto";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Registrar(obj, out Mensaje);
+            }
+
+            return 0;
+        }
+
+
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombre) || string.IsNullOrWhiteSpace(obj.Nombre))
+            {
+                Mensaje = "El nombre del producto no puede ser vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripción del producto no puede ser vacío";
+            }
+            else if (obj.oMarca.IdMarca == 0)
+            {
+                Mensaje = "Debe seleccionar una Marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Debe seleccionar una Categoria";
+            }
+            else if (obj.Precio == 0)
+            {
+                Mensaje = "Debe ingresar el precio del producto";
+            }
+            else if (obj.Stock == 0)
+            {
+                Mensaje = "Debe ingresar el Stock del producto";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool GuardarDatosImagen(Producto obj, out string Mensaje)
+        {
+            return objCapaDato.GuardarDatosImagen(obj, out Mensaje);
+        }
+
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+
+    }
+}
